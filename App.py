41b9a450d3249291a1e6b7d2bad362630d1e735d
yashwanth192006gowda-cyber <yashwanth192006gowda@gmail.com>
2026-04-15 14:17:54 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,4 +3,4 @@ from transformers import pipeline
 Load the summarization model
 est.cache_resource
 def load summarizer():
-return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+  return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
