f6844c4245868f12e66d904dd58854365c0fb5ee
yashwanth192006gowda-cyber <yashwanth192006gowda@gmail.com>
2026-04-15 14:18:14 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from transformers import pipeline
-Load the summarization model
 est.cache_resource
 def load summarizer():
   return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
