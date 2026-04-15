7ab052641d6e0e0cb6d7e429f3e904f749141d06
yashwanth192006gowda-cyber <yashwanth192006gowda@gmail.com>
2026-04-15 15:06:56 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from transformers import pipeline
-est.cache_resource
-def load summarizer():
-  return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-summarizer = load summarizer()
+@st.cache_resource
+def load_summarizer():
+    return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 st.title(" AI Text Summarizer")
 st.write("Enter a long text below, and get a concise summary!")
-long_text = st.text_area("Enter text to sumarize:", height=200)
+long_text=st.text_area("Enter text to summarize:",height=200)
 max_length = st.slider("Max Summary Length", min_value=50,
                        max_value=300, value=130)
 min_length = st.slider("Min Summary Length", min_value=20,
@@ -14,7 +13,8 @@ min_length = st.slider("Min Summary Length", min_value=20,
 if st.button("Summarize"):
     if long_text.strip():
         with st.spinner("Generating summary... "):
-            summary = load summarizer(long_text, max_length=max_length, min_length=min_length, do_sample=False)
+            summary = summarizer(long_text, max_length=max_length,
+                                 min_length=min_length, do_sample=False)
         st.subheader(" Summary:")
         st.success(summary[0]['summary_text'])
     else:
